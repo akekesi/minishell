249871f093d285db18e685760a0093d84f0c5712
akekesi <kekesi.att@gmail.com>
2024-01-12 22:31:45 +0100

    test: created dummy test

---
+++ test/test_main.py
@@ -0,0 +1,15 @@
+import sys
+import unittest
+
+sys.path.append("..")
+from src.main import main
+
+
+class TestMain(unittest.TestCase):
+
+    def test_main(self):
+        self.assertEqual(main(), "Hello, World!", "Return value shuld be Hello, World!")
+
+
+if __name__ == "__main__":
+    unittest.main()
