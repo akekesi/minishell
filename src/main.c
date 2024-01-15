fdfb7e3d10e1d8c34a2c44d29ee15e1468c3f4fb
Takayuki Nakajo <tnakajo@c4c6c2.42berlin.de>
2024-01-15 19:10:03 +0100

    managed ''

--- src/main.c
+++ src/main.c
@@ -5,9 +5,9 @@ void	prompt()
 	printf("minishell> ");
 }
 
-char*	read_input()
+char	*read_input()
 {
-	char* buffer = malloc(MAX_INPUT_SIZE);
+	char	*buffer = malloc(MAX_INPUT_SIZE);
 	if (!buffer)
 	{
 		printf("malloc error");
@@ -24,48 +24,6 @@ char*	read_input()
 
 	return buffer;
 }
-
-// char**	parse_input(char* input)
-// {
-// 	char** args;
-// 	int i;
-// 	int j;
-// 	int k;
-	
-// 	args = (char **)malloc(MAX_ARG_SIZE * sizeof(char*));
-// 	if (!args) {
-// 		printf("malloc error");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	for (i = 0; i < MAX_ARG_SIZE; i++) {
-//         args[i] = (char *)malloc(MAX_ARG_SIZE * sizeof(char)); // Allocate memory for each argument
-//         if (!args[i]) {
-//             printf("malloc error\n");
-//             exit(EXIT_FAILURE);
-//         }
-//     }
-	
-// 	i = 0;
-// 	j = 0;
-// 	k = 0;
-// 	while (input[i])
-// 	{
-// 		if (input[i] != ' ')
-// 		{
-// 			args[j][k] = input[i];
-// 			k++;
-// 		}
-// 		else if (input[i] == ' ' && input[i + 1] != ' ')
-// 		{
-// 			args[j][k] = '\0';
-// 			j++;
-// 			k = 0;
-// 		}
-// 		i++;
-// 	}
-// 	args[i] = NULL;
-// 	return args;
-// }
 
 void	execute_command(char **cmd)
 {
@@ -100,14 +58,8 @@ void	signal_handler(int sig)
 int	main(int ac, char **av, char **envp)
 {
 	char	*input;
-	// char	**cmd;
 	t_words	*cmd;
-
-	// envp ??
-    // for (int i = 0; envp[i] != NULL; i++) {
-    //     printf("%s\n", envp[i]);
-    // }
-
+	
 	signal(SIGINT, signal_handler);
 	signal(SIGQUIT, SIG_IGN);
 	while (42)
@@ -115,20 +67,11 @@ int	main(int ac, char **av, char **envp)
 		prompt();
 		input = read_input();
 		cmd = parse_input(input);
-		// printf("%s\n", cmd->value);
-		// printf("%s\n", cmd->prev->value);
-		if (cmd)
+		while (cmd != NULL)
 		{
-			printf("%s\n", cmd->value);
-			cmd++;
-			printf("%s\n", cmd[1].value);
-			cmd++;
-			printf("%s\n", cmd[2].value);
+			printf("-->%s\n", cmd->value);
+			cmd = cmd->next;
 		}
-		// printf("%s\n", cmd[1].next->value);
-		// printf("%s\n", cmd[1].prev->value);
-		// Example: Execute the command
-		// printf("You typed: %s\n", *cmd);
 		// execute_command(cmd);
 		// free(input);
 		// free(cmd);
--- src/parse.c
+++ src/parse.c
@@ -8,20 +8,29 @@ t_words *parse_input(char* input)
 	char buffer[MAX_ARG_SIZE];
 	int i;
 	int	j;
+	int	quote_flag;
 
 	head = NULL;
 	current = NULL;
 	new_node = (t_words *)malloc(sizeof(t_words));
 	i = 0;
 	j = 0;
+	quote_flag = 0;
 	while (input[i])
 	{
-		if (input[i] != ' ')
+		if (input[i] == '\'')
+		{
+			if (quote_flag == 1)
+				quote_flag = 0;
+			else
+				quote_flag = 1;
+		}
+		else if (input[i] != ' ' || quote_flag == 1)
 		{
 			buffer[j] = input[i];
 			j++;
 		}
-		else if (j > 0)
+		else if (j > 0 && quote_flag == 0)
 		{
 			buffer[j] = '\0';	
 			new_node = (t_words *)malloc(sizeof(t_words));
@@ -29,21 +38,14 @@ t_words *parse_input(char* input)
             new_node->prev = current;
             new_node->next = NULL;
 			if (current)
-			{
 				current->next = new_node;
-				// new_node->prev = current;
-			}
 			else
-			{
 				head = new_node;
-				// new_node->prev = new_node;
-			}
 			current = new_node;
 			j = 0;
 		}
 		i++;
 	}
-	// head->prev = current;
 	// Handle the last word
 	if (j > 0)
 	{
@@ -51,20 +53,11 @@ t_words *parse_input(char* input)
 		new_node = (t_words *)malloc(sizeof(t_words));
 		new_node->value = strdup(buffer);
         new_node->prev = current;
-        // new_node->next = head;
         new_node->next = NULL;
-		// if (j == i)
-		// {
-		// 	new_node->next = new_node;
-		// 	new_node->prev = new_node;
-		// }
 		if (current)
 			current->next = new_node;
 		else
 			head = new_node; // In case input is a single word
-		// printf("hi");
-
-		// printf("%s\n", head->value);
 	}
 	return head;
 }
